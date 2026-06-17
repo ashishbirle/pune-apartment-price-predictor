668e1a23cab3f875de7e7485f8071ec0b533297b
Ashish Birle <ashish.birle13@gmail.com>
2026-06-17 16:10:46 +0530

    Update parameters in Random Forest model

--- train_random_forest.py
+++ train_random_forest.py
@@ -25,7 +25,7 @@ y = df.price
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-model = RandomForestRegressor()
+model = RandomForestRegressor(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
 
 print("The model scored", model.score(X_test, y_test))
